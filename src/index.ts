87cac20672551432af02a85f2190b5e07d69140e
e1ha <haelaine6@gmail.com>
2022-07-11 10:48:58 -0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { ICommandPalette } from '@jupyterlab/apputils';
+
+/**
+ * Initialization data for the jupyterlab_apod extension.
+ */
+const extension: JupyterFrontEndPlugin<void> = {
+  id: 'jupyterlab-apod',
+  autoStart: true,
+  requires: [ICommandPalette],
+  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
+    console.log('JupyterLab extension jupyterlab_apod is activated!');
+    console.log('ICommandPalette:', palette);
+  }
+};
+
+export default extension;
